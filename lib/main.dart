dfbafa1fbdcf639686debfcbe461e3998e8c7b00
aochuih <aochuih@hotmail.com>
2019-04-20 23:04:06 +0800

    appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
+import 'package:flutter/cupertino.dart';
 
 void main() {
   debugPaintSizeEnabled = false; // Set to true for visual layout
@@ -11,157 +12,46 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter layout demo',
-      home: buildHomePage('Strawberry Pavlova Recipe'),
+      home: buildHomePage(''),
     );
   }
 
+  void _morePressed(TapUpDetails details) {
+    print('more pressed');
+  }
+
   Widget buildHomePage(String title) {
-    final titleText = Container(
-      padding: EdgeInsets.all(20),
-      child: Text(
-        'Strawberry Pavlova',
-        style: TextStyle(
-          fontWeight: FontWeight.w900,
-          letterSpacing: 0.5,
-          fontSize: 30,
-        ),
-      ),
-    );
-
-    final subTitle = Text(
-      'Pavlova is a meringue-based dessert named after the Russian ballerina '
-          'Anna Pavlova. Pavlova features a crisp crust and soft, light inside, '
-          'topped with fruit and whipped cream.',
-      textAlign: TextAlign.center,
-      style: TextStyle(
-        fontFamily: 'Georgia',
-        fontSize: 25,
-      ),
-    );
-
-    // #docregion ratings, stars
-    var stars = Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Icon(Icons.star, color: Colors.green[500]),
-        Icon(Icons.star, color: Colors.green[500]),
-        Icon(Icons.star, color: Colors.green[500]),
-        Icon(Icons.star, color: Colors.black),
-        Icon(Icons.star, color: Colors.black),
-      ],
-    );
-    // #enddocregion stars
-
-    final ratings = Container(
-      padding: EdgeInsets.all(20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          stars,
-          Text(
-            '170 Reviews',
-            style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.w800,
-              fontFamily: 'Roboto',
-              letterSpacing: 0.5,
-              fontSize: 20,
-            ),
-          ),
-        ],
-      ),
-    );
-    // #enddocregion ratings
-
-    // #docregion iconList
-    final descTextStyle = TextStyle(
-      color: Colors.black,
-      fontWeight: FontWeight.w800,
-      fontFamily: 'Roboto',
-      letterSpacing: 0.5,
-      fontSize: 18,
-      height: 2,
-    );
-
-    // DefaultTextStyle.merge() allows you to create a default text
-    // style that is inherited by its child and all subsequent children.
-    final iconList = DefaultTextStyle.merge(
-      style: descTextStyle,
-      child: Container(
-        padding: EdgeInsets.all(20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Column(
-              children: [
-                Icon(Icons.kitchen, color: Colors.green[500]),
-                Text('PREP:'),
-                Text('25 min'),
-              ],
-            ),
-            Column(
-              children: [
-                Icon(Icons.timer, color: Colors.green[500]),
-                Text('COOK:'),
-                Text('1 hr'),
-              ],
-            ),
-            Column(
-              children: [
-                Icon(Icons.restaurant, color: Colors.green[500]),
-                Text('FEEDS:'),
-                Text('4-6'),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-    // #enddocregion iconList
-
-    // #docregion leftColumn
-    final leftColumn = Container(
-      padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
-      child: Column(
-        children: [
-          titleText,
-          subTitle,
-          ratings,
-          iconList,
-        ],
-      ),
-    );
-    // #enddocregion leftColumn
-
-    final mainImage = Image.asset(
-      'assets/pavlova.jpg',
-      fit: BoxFit.cover,
-    );
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      // #docregion body
-      body: Center(
-        child: Container(
-          margin: EdgeInsets.fromLTRB(0, 40, 0, 30),
-          height: 600,
-          child: Card(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  child: leftColumn,
+      backgroundColor: Colors.white,
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            pinned: true,
+            expandedHeight: 326,
+            actions: <Widget>[
+              GestureDetector(
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                  width: 40,
+                  height: 40,
+                  child: Image.asset('assets/more.png'),
                 ),
-                Expanded(
-                  child: mainImage,
-                )
-              ],
+                onTapUp: _morePressed,
+              )
+            ],
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(title),
+              background: Stack(
+                fit: StackFit.expand,
+                children: <Widget>[
+                  Image.asset('assets/pavlova.jpg',
+                    fit: BoxFit.cover,),
+                ],
+              ),
             ),
-          ),
-        ),
-      ),
+          )
+        ],
+      )
     );
   }
 }
